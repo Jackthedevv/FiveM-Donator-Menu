c52b2681a23f6d93a4b873990205527ca4452731
Jack Devolpment <61220317+Jackthedevv@users.noreply.github.com>
2024-05-05 05:25:13 -0600

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,21 @@
+Config = {
+    menuPos = {x = 1450, y = 100},
+    
+    useKeyBind = true,
+    menuKey = 167, -- only works if userKeyBind is true
+    menuCommand = "donatormenu", -- only works if userKeyBind is false
+
+    useMenuTitle = false, -- i recommend having off if ur using a menuTexture
+    menuTitle = "~p~Donator ~y~Menu", -- only works if useMenuTitle is true
+    useMenuDescription = true,
+    menuDescription = "Welcome to the Donator Menu", -- only works if useMenuTitle is true
+
+    useImageBanner = true,
+    AddonMenuBannerImage = 'https://cdn.discordapp.com/attachments/1161069645827166304/1227890264597856297/3dp4fyzfl7q51.gif?ex=662a0cec&is=661797ec&hm=d4992594e6df2a555ae55724cc67c9f69ecb22bc0627774b17df374ed4de8b24&',
+
+    aces = {
+        donatorPack1 = "donator.pack1",
+        freeCarPack = "donator.FreeCars"
+        -- add more aces if needed
+    }
+}
